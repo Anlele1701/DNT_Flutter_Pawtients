a3f476421878af6e3ab50d497cd8763109bd645d
Anlele1701 <lethanhduyan@gmail.com>
2024-06-15 10:40:14 +0700

    Fixed UIUX LoginScreen

--- frontend_cl/lib/view/login_screen.dart
+++ frontend_cl/lib/view/login_screen.dart
@@ -24,44 +24,44 @@ class _LoginScreenState extends State<LoginScreen>
   void initState() {
     super.initState();
     _controller =
-        AnimationController(duration: const Duration(seconds: 8), vsync: this);
+        AnimationController(duration: const Duration(seconds: 4), vsync: this);
     //? Top Animation
     _topAlignmentAnimation = TweenSequence<Alignment>([
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.topLeft, end: Alignment.topRight),
-          weight: 2),
+          weight: 1),
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.topRight, end: Alignment.bottomRight),
-          weight: 2),
+          weight: 1),
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.bottomRight, end: Alignment.bottomLeft),
-          weight: 2),
+          weight: 1),
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
-              begin: Alignment.bottomLeft, end: Alignment.topRight),
-          weight: 2),
+              begin: Alignment.bottomLeft, end: Alignment.topLeft),
+          weight: 1),
     ]).animate(_controller);
     //? Bottom animation
     _bottomAlignmentAnimation = TweenSequence<Alignment>([
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.bottomRight, end: Alignment.bottomLeft),
-          weight: 2),
+          weight: 1),
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.bottomLeft, end: Alignment.topLeft),
-          weight: 2),
+          weight: 1),
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.topLeft, end: Alignment.topRight),
-          weight: 2),
+          weight: 1),
       TweenSequenceItem<Alignment>(
           tween: Tween<Alignment>(
               begin: Alignment.topRight, end: Alignment.bottomRight),
-          weight: 2),
+          weight: 1),
     ]).animate(_controller);
     _controller.repeat();
   }
@@ -74,206 +74,217 @@ class _LoginScreenState extends State<LoginScreen>
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Container(
-      width: double.infinity,
-      height: double.infinity,
-      child: Stack(
-        children: [
-          AnimatedBuilder(
-            animation: _controller,
-            builder: (context, child) => ClipPath(
-              clipper: UpperWaveClipper(),
-              child: Container(
-                height: 241,
-                decoration: BoxDecoration(
-                    gradient: LinearGradient(
-                        colors: const [
-                      Color(0xffFF810B),
-                      Color(0xffFB6D48),
+        body: AnimatedBuilder(
+      animation: _controller,
+      builder: (context, child) => Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: const [Color(0xffFF7B00), Color(0xffF9B26F)],
+                begin: _topAlignmentAnimation.value,
+                end: _bottomAlignmentAnimation.value)),
+        child: Stack(
+          children: [
+            // AnimatedBuilder(
+            //   animation: _controller,
+            //   builder: (context, child) => ClipPath(
+            //     clipper: UpperWaveClipper(),
+            //     child: Container(
+            //       height: 241,
+            //       decoration: BoxDecoration(
+            //           gradient: LinearGradient(
+            //               colors: const [
+            //             Color(0xffFF810B),
+            //             Color(0xffFB6D48),
+            //           ],
+            //               begin: _topAlignmentAnimation.value,
+            //               end: _bottomAlignmentAnimation.value)),
+            //     ),
+            //   ),
+            // ),
+            // Positioned(
+            //   bottom: 0,
+            //   left: 0,
+            //   right: 0,
+            //   child: AnimatedBuilder(
+            //     animation: _controller,
+            //     builder: (context, child) => ClipPath(
+            //       clipper: DownWaveClipper(),
+            //       child: Container(
+            //         height: 400,
+            //         decoration: BoxDecoration(
+            //             gradient: LinearGradient(
+            //                 colors: const [
+            //               Color(0xffFF810B),
+            //               Color(0xffFB6D48),
+            //             ],
+            //                 begin: _topAlignmentAnimation.value,
+            //                 end: _bottomAlignmentAnimation.value)),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+            Positioned(
+              top: screenHeight * 0.1,
+              left: 50,
+              right: 50,
+              child: GlassmorphicContainer(
+                width: 350,
+                height: screenHeight * 0.8,
+                borderRadius: 50,
+                blur: 45,
+                alignment: Alignment.bottomCenter,
+                border: 2,
+                linearGradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      const Color(0xFFffffff).withOpacity(0.4),
+                      const Color(0xFFffffff).withOpacity(0.4),
                     ],
-                        begin: _topAlignmentAnimation.value,
-                        end: _bottomAlignmentAnimation.value)),
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: AnimatedBuilder(
-              animation: _controller,
-              builder: (context, child) => ClipPath(
-                clipper: DownWaveClipper(),
-                child: Container(
-                  height: 400,
-                  decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                          colors: const [
-                        Color(0xffFF810B),
-                        Color(0xffFB6D48),
-                      ],
-                          begin: _topAlignmentAnimation.value,
-                          end: _bottomAlignmentAnimation.value)),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            top: 140,
-            left: 50,
-            right: 50,
-            child: GlassmorphicContainer(
-              width: 350,
-              height: 650,
-              borderRadius: 50,
-              blur: 45,
-              alignment: Alignment.bottomCenter,
-              border: 2,
-              linearGradient: LinearGradient(
+                    stops: const [
+                      0.1,
+                      1,
+                    ]),
+                borderGradient: LinearGradient(
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight,
                   colors: [
-                    const Color(0xFFffffff).withOpacity(0.4),
-                    const Color(0xFFffffff).withOpacity(0.4),
+                    const Color(0xFFffffff).withOpacity(0.35),
+                    const Color((0xFFFFFFFF)).withOpacity(0.35),
                   ],
-                  stops: [
-                    0.1,
-                    1,
-                  ]),
-              borderGradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  const Color(0xFFffffff).withOpacity(0.35),
-                  const Color((0xFFFFFFFF)).withOpacity(0.35),
-                ],
-              ),
-              child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 65, bottom: 55),
-                    child: const ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(130)),
-                      child: Image(
-                        image: AssetImage('assets/images/pawtient_logo.png'),
-                        height: 200,
-                        width: 200,
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 65, bottom: 55),
+                      child: const ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(130)),
+                        child: Image(
+                          image: AssetImage('assets/images/pawtient_logo.png'),
+                          height: 200,
+                          width: 200,
+                        ),
                       ),
                     ),
-                  ),
-                  const TextFieldLogin(
-                    labelInput: "Email",
-                    iconInput: Icons.email_outlined,
-                    textObscure: false,
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  SizedBox(
-                    height: 44,
-                    width: 274,
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 14,
-                              color: const Color(0xff000000).withOpacity(.25),
-                              offset: const Offset(0, 0))
-                        ],
-                        borderRadius: BorderRadius.circular(22),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.only(left: 14),
-                        child: TextField(
-                            obscureText: _obscured,
-                            focusNode: textFieldFocusNode,
-                            decoration: InputDecoration(
-                              floatingLabelBehavior:
-                                  FloatingLabelBehavior.never,
-                              isDense: true,
-                              hintStyle: TextStyle(
-                                  color:
-                                      const Color(0xff000000).withOpacity(.5),
-                                  fontSize: 14),
-                              hintText: "Password",
-                              icon: const Icon(Icons.lock_outlined,
-                                  color: Color(0xff808080), size: 23),
-                              suffixIcon: Padding(
-                                padding: const EdgeInsets.fromLTRB(0, 0, 4, 0),
-                                child: GestureDetector(
-                                  onTap: _toggleObscured,
-                                  child: Icon(
-                                      _obscured
-                                          ? Icons.visibility_rounded
-                                          : Icons.visibility_off_rounded,
-                                      size: 24,
-                                      color: const Color(0xff808080)),
+                    const TextFieldLogin(
+                      labelInput: "Email",
+                      iconInput: Icons.email_outlined,
+                      textObscure: false,
+                    ),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    SizedBox(
+                      height: 44,
+                      width: 274,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 14,
+                                color: const Color(0xff000000).withOpacity(.25),
+                                offset: const Offset(0, 0))
+                          ],
+                          borderRadius: BorderRadius.circular(22),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 14),
+                          child: TextField(
+                              obscureText: _obscured,
+                              focusNode: textFieldFocusNode,
+                              decoration: InputDecoration(
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.never,
+                                isDense: true,
+                                hintStyle: TextStyle(
+                                    color:
+                                        const Color(0xff000000).withOpacity(.5),
+                                    fontSize: 14),
+                                hintText: "Password",
+                                icon: const Icon(Icons.lock_outlined,
+                                    color: Color(0xff808080), size: 23),
+                                suffixIcon: Padding(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 0, 4, 0),
+                                  child: GestureDetector(
+                                    onTap: _toggleObscured,
+                                    child: Icon(
+                                        _obscured
+                                            ? Icons.visibility_rounded
+                                            : Icons.visibility_off_rounded,
+                                        size: 24,
+                                        color: const Color(0xff808080)),
+                                  ),
                                 ),
-                              ),
-                              border: InputBorder.none,
-                            )),
+                                border: InputBorder.none,
+                              )),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(height: 10),
-                  const Text(
-                    "Quên mật khẩu?",
-                    style: TextStyle(color: Color(0xff723700), fontSize: 12),
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    width: 167,
-                    height: 47,
-                    decoration: BoxDecoration(
-                        borderRadius:
-                            const BorderRadius.all(Radius.circular(30)),
-                        boxShadow: [
-                          BoxShadow(
-                              blurRadius: 4,
-                              color: const Color(0xff000000).withOpacity(.25),
-                              offset: const Offset(0, 4))
+                    const SizedBox(height: 10),
+                    const Text(
+                      "Quên mật khẩu?",
+                      style: TextStyle(color: Color(0xff723700), fontSize: 12),
+                    ),
+                    const SizedBox(height: 20),
+                    Container(
+                      width: 167,
+                      height: 47,
+                      decoration: BoxDecoration(
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(30)),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 4,
+                                color: const Color(0xff000000).withOpacity(.25),
+                                offset: const Offset(0, 4))
+                          ]),
+                      child: FilledButton(
+                        onPressed: () {},
+                        style: const ButtonStyle(
+                          backgroundColor: MaterialStatePropertyAll<Color>(
+                              Color(0xffFF810B)),
+                        ),
+                        child: const Text("Đăng nhập",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Bạn chưa có tài khoản?",
+                              style: TextStyle(
+                                  color: Color(0xff474747), fontSize: 12)),
+                          Text(" Đăng ký",
+                              style: TextStyle(
+                                  color: Color(0xff723700), fontSize: 12))
                         ]),
-                    child: FilledButton(
-                      onPressed: () {},
-                      style: const ButtonStyle(
-                        backgroundColor:
-                            MaterialStatePropertyAll<Color>(Color(0xffFF810B)),
-                      ),
-                      child: const Text("Đăng nhập",
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold)),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text("Bạn chưa có tài khoản?",
+                    Expanded(
+                      child: Align(
+                          alignment: const Alignment(0.05, 0.6),
+                          child: Text(
+                            "Copyright © 2024 Pawtient",
                             style: TextStyle(
-                                color: Color(0xff474747), fontSize: 12)),
-                        Text(" Đăng ký",
-                            style: TextStyle(
-                                color: Color(0xff723700), fontSize: 12))
-                      ]),
-                  Expanded(
-                    child: Align(
-                        alignment: const Alignment(0.05, 0.6),
-                        child: Text(
-                          "Copyright © 2024 Pawtient",
-                          style: TextStyle(
-                              color: const Color(0xffFFFFFF).withOpacity(.6),
-                              fontSize: 12),
-                        )),
-                  )
-                ],
+                                color: const Color(0xffFFFFFF).withOpacity(.6),
+                                fontSize: 12),
+                          )),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     ));
   }
