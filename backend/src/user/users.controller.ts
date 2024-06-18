d816c5c4e20099a8c474d41e58c44e3a5a207d02
Anlele1701 <lethanhduyan@gmail.com>
2024-06-18 13:56:17 +0700

    Rename file
    Added login regex

--- backend/src/user/users.controller.ts
+++ backend/src/user/users.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
 import { UsersService } from "./users.service";
-import { CreateUserDto } from "./dto/CreateUser.dto";
+import { RegisterUserDto } from "./dto/RegisterUser.dto";
 import { User } from "src/schemas/User.schema";
 
 @Controller('users')
@@ -8,7 +8,7 @@ export class UsersController{
     constructor(private usersService: UsersService){}
     @Post()
     @UsePipes(new ValidationPipe())
-    createUser(@Body() createUserDto: CreateUserDto):Promise<User>{
+    createUser(@Body() createUserDto: RegisterUserDto):Promise<User>{
         return this.usersService.createUser(createUserDto);
     }
 }
