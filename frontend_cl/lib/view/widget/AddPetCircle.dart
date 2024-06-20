ca8213a52d76d93c81340b18faeef7bea8abcd8c
Anlele1701 <lethanhduyan@gmail.com>
2024-06-20 23:51:07 +0700

    Completed Pet Profile Screen
    - Fix responsive bugs on different screens
    - Completed UI/UX
    - Added ripple effect for ListTile

--- frontend_cl/lib/view/widget/AddPetCircle.dart
+++ frontend_cl/lib/view/widget/AddPetCircle.dart
@@ -20,20 +20,30 @@ class _AddPetCircleState extends State<AddPetCircle> {
               shape: BoxShape.circle,
               color: const Color(0xffF9B26F),
             ),
-            child: Center(
-              child: Container(
-                height: 42,
-                width: 42,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                        color: Color.fromARGB(255, 255, 255, 255), width: 2)),
-                child: Padding(
-                    padding: EdgeInsets.all(2),
-                    child: Icon(
-                      Icons.add,
-                      color: Colors.white,
-                    )),
+            child: Material(
+              color: Colors.transparent,
+              shape: const CircleBorder(),
+              child: InkWell(
+                splashColor: Color(0xffF48B29),
+                onTap: () {},
+                customBorder: const CircleBorder(),
+                child: Center(
+                  child: Container(
+                    height: 42,
+                    width: 42,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                            color: Color.fromARGB(255, 255, 255, 255),
+                            width: 2)),
+                    child: Padding(
+                        padding: EdgeInsets.all(2),
+                        child: Icon(
+                          Icons.add,
+                          color: Colors.white,
+                        )),
+                  ),
+                ),
               ),
             )),
         Text("Tạo hồ sơ")
