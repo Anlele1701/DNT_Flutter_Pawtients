4e33f8d131527a73a38ae6e8d53f2211aba34032
daibangdangcap <nguyentienthanh1122003@gmail.com>
2024-06-23 20:49:51 +0700

    Complete Create New Pet Service Function

--- frontend_ad/lib/api_services/pet_s_service.dart
+++ frontend_ad/lib/api_services/pet_s_service.dart
@@ -1,3 +1,21 @@
+import 'package:dio/dio.dart';
+import 'package:frontend_ad/api_services/api_urls.dart';
+import 'package:frontend_ad/models/pet_service.dart';
+
 class Pet_S_Service{
-  
+  Future<PetService?> createNewPetService(PetService petService)async{
+    final Dio dio= Dio();
+    try {
+      final data = petService.toJson();
+      Response response = await dio.post(
+        ApiUrls.PAWTIENT_URL+'/pet-service/create-new-pet-service',
+        data: data,
+      );
+      print(response.data);
+      return PetService.fromJson(response.data);
+    } catch (e) {
+      print('Exception: $e');
+      return null;
+    }
+  }
 }
--- frontend_ad/lib/views/web_views/create_pet_service.dart
+++ frontend_ad/lib/views/web_views/create_pet_service.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:frontend_ad/models/pet_service.dart';
+import 'package:frontend_ad/views_models/pet_service_view_model.dart';
+import 'package:provider/provider.dart';
 
 class CreatePetService extends StatefulWidget {
   const CreatePetService({super.key});
@@ -8,29 +11,44 @@ class CreatePetService extends StatefulWidget {
 }
 
 class _MyWidgetState extends State<CreatePetService> {
+  final TextEditingController tenDichVuController=TextEditingController();
+  final TextEditingController moTaController=TextEditingController();
+  final TextEditingController giaTienController= TextEditingController();
+  PetService petService= PetService();
+  final PetServiceViewModel petServiceViewModel=PetServiceViewModel();
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
         children: [
           TextField(
-            decoration: InputDecoration(
+            controller: tenDichVuController,
+            decoration: const InputDecoration(
               labelText: "Tên dịch vụ",
               labelStyle: TextStyle(color: Colors.black)
             ),
           ),
           TextField(
-            decoration: InputDecoration(
+            controller: moTaController,
+            decoration: const InputDecoration(
               labelText: "Mô tả",
               labelStyle: TextStyle(color: Colors.black)
             ),
           ),
           TextField(
-            decoration: InputDecoration(
+            controller: giaTienController,
+            decoration: const InputDecoration(
               labelText: "Giá tiền",
               labelStyle: TextStyle(color: Colors.black)
             ),
-          )
+          ),
+          TextButton(onPressed: (){
+            petService.tenDichVu=tenDichVuController.text;
+            petService.moTa=moTaController.text;
+            petService.giaTien=int.tryParse(giaTienController.text);
+            petServiceViewModel.createNewPetService(petService);
+            
+          }, child: Text("Xác nhận"))
         ],
       ),
     );
