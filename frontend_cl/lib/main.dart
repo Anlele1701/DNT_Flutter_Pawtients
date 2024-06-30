798c42e7e8d9f6981e153f0d5aebc4df9b4a3bf1
HUYPHOENIX <truongquanghuy2003@gmail.com>
2024-07-01 00:06:49 +0700

    Complete medicine list (missing search bar)

--- frontend_cl/lib/main.dart
+++ frontend_cl/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/view/login_screen.dart';
+import 'package:frontend/view/widget/Dich_Vu/service_page.dart';
 import 'package:frontend/view/widget/Dich_Vu/service_salon.dart';
 import 'package:frontend/view/petprofile_screen.dart';
+import 'package:frontend/view/widget/Products/List_products.dart';
 import 'package:frontend/view/widget/spash_screen.dart';
 
 void main() {
@@ -14,6 +16,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: SalonService());
+        debugShowCheckedModeBanner: false, home: Products());
   }
 }
---
+++ frontend_cl/lib/view/widget/Products/content_filter.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'package:chips_choice/chips_choice.dart';
+import 'package:animated_checkmark/animated_checkmark.dart';
+
+List<String> tags = [''];
+
+List<String> options = ['Kháng sinh', 'Tiêu hóa', 'Dinh dưỡng', 'Sốt', 'Khác'];
+
+bool selected = false;
+bool showChips = false;
+bool isIconRotated = false;
+
+//Custom UI cho chíp
+class CustomChip extends StatelessWidget {
+  final String label;
+  final bool selected;
+  final Function(bool selected) onSelect;
+  const CustomChip({
+    Key? key,
+    required this.label,
+    this.selected = false,
+    required this.onSelect,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.only(left: 5),
+        child: FilterChip(
+          label: Text(
+            label,
+            style: TextStyle(
+                color: selected ? Color(0xFFFFFFFF) : Color(0xFF534F4F)),
+          ),
+          selected: selected,
+          onSelected: onSelect,
+          selectedColor: Colors.amber[800],
+          backgroundColor: Colors.white,
+          checkmarkColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0), // Set border radius
+            side: BorderSide(
+                color: selected ? Color(0xFFFFFFFF) : Color(0xFF534F4F),
+                width: 0.5 // Border color
+                ),
+          ),
+        ));
+  }
+}
+
+Widget GradientColor() {
+  return Positioned.fill(
+      child: Container(
+    decoration: const BoxDecoration(
+        gradient: LinearGradient(
+      colors: [Color(0xFFF1B237), Color(0xFFFB7E09)],
+      stops: [0.12, 1],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    )),
+  ));
+}
+
+//Show ra các chips
+class MyChips extends StatefulWidget {
+  const MyChips({super.key});
+
+  @override
+  State<MyChips> createState() => _MyChipsState();
+}
+
+class _MyChipsState extends State<MyChips> {
+  @override
+  Widget build(BuildContext context) {
+    return ChipsChoice<String>.multiple(
+      value: tags,
+      // mốt viết hàm ra filter tại đây
+      onChanged: (val) => setState(() {
+        tags = val;
+        for (int i = 0; i < tags.length; i++) {
+          print(tags[i]);
+        }
+      }),
+      choiceItems: C2Choice.listFrom<String, String>(
+        source: options,
+        value: (i, v) => v,
+        label: (i, v) => v,
+      ),
+      choiceBuilder: (item, i) {
+        return CustomChip(
+          label: item.label,
+          selected: item.selected,
+          onSelect: item.select!,
+        );
+      },
+    );
+  }
+}
+Widget MyButton() {
+  return GestureDetector(
+    onTap: () {
+      },
+    child: Container(
+      width: 25,
+      height: 25,
+      decoration: BoxDecoration(
+        color: Color(0xFFFF7B00),
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: 
+      Icon(
+        Icons.add,
+        size: 18,
+        color: Colors.white,
+        ),
+    ),
+  );
+}
